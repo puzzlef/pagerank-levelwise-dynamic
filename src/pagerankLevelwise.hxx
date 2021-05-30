814066aa77e70813d1c4403cd95ea253044aac5d
Subhajit Sahu <wolfram77@gmail.com>
2021-05-30 20:00:05 +0530

    :skunk: w/ pagerankLevelwise skip-comp

--- src/pagerankLevelwise.hxx
+++ src/pagerankLevelwise.hxx
@@ -26,33 +26,49 @@ auto pagerankComponents(const G& x, const H& xt, const PagerankOptions<T>& o) {
 }
 
 
+template <class G, class C>
+auto pagerankComponentSizes(const G& w, const C& wcs, const G& x, const C& xcs) {
+  vector<int> a; bool fail = false;
+  for (int i=0, W=wcs.size(), X=xcs.size(); i<X; i++) {
+    if (!fail && i<W && componentsEqual(w, wcs[i], x, xcs[i])) a.push_back(-xcs[i].size());
+    else { a.push_back(xcs[i].size()); fail = true; }
+  }
+  return a;
+}
+
+
 template <class T, class J>
 int pagerankLevelwiseLoop(vector<T>& a, vector<T>& r, vector<T>& c, const vector<T>& f, const vector<int>& vfrom, const vector<int>& efrom, const vector<int>& vdata, J&& ns, int N, T p, T E, int L) {
   int v = 0; float l = 0;
   for (int n : ns) {
-    int k = pagerankMonolithicLoop(a, r, c, f, vfrom, efrom, vdata, v, v+n, N, p, E * (float(n)/N), L);
+    if (n<0) { v += -n; continue; }
+    l += pagerankMonolithicLoop(a, r, c, f, vfrom, efrom, vdata, v, v+n, N, p, E * (float(n)/N), L) * (float(n)/N);
     swap(a, r);
-    l += k * (float(n)/N);
     v += n;
   }
+  swap(a, r);
   return int(l);
 }
 
 
-// Find pagerank of components in topological order (pull, CSR).
-// @param xt transpose graph, with vertex-data=out-degree
+// Find pagerank of components in topological order (pull, CSR, skip-comp).
+// @param w  previous graph
+// @param wt previous transpose graph, with vertex-data=out-degree
+// @param x  current graph
+// @param xt current transpose graph, with vertex-data=out-degree
 // @param q initial ranks (optional)
 // @param o options {damping=0.85, tolerance=1e-6, maxIterations=500}
 // @returns {ranks, iterations, time}
 template <class G, class H, class T=float>
-PagerankResult<T> pagerankLevelwise(const G& x, const H& xt, const vector<T> *q=nullptr, PagerankOptions<T> o={}) {
+PagerankResult<T> pagerankLevelwise(const G& w, const H& wt, const G& x, const H& xt, const vector<T> *q=nullptr, PagerankOptions<T> o={}) {
   T    p = o.damping;
   T    E = o.tolerance;
   int  L = o.maxIterations, l;
   int  N = xt.order();
-  auto cs = pagerankComponents(x, xt, o);
-  auto ks = join(cs);
-  auto ns = transform(cs, [](const auto& c) { return c.size(); });
+  auto wcs = pagerankComponents(w, wt, o);
+  auto xcs = pagerankComponents(x, xt, o);
+  auto ns = pagerankComponentSizes(w, wcs, x, xcs);
+  auto ks = join(xcs);
   auto vfrom = sourceOffsets(xt, ks);
   auto efrom = destinationIndices(xt, ks);
   auto vdata = vertexData(xt, ks);
@@ -62,8 +78,21 @@ PagerankResult<T> pagerankLevelwise(const G& x, const H& xt, const vector<T> *q=
     fill(a, T());
     if (q) copy(r, qc);
     else fill(r, T(1)/N);
-    mark([&] { pagerankFactor(f, vfrom, efrom, vdata, 0, N, N, p); });
+    mark([&] { pagerankFactor(f, vfrom, efrom, vdata, 0, N, N, p); multiply(c, r, f); copy(a, r); });
     mark([&] { l = pagerankLevelwiseLoop(a, r, c, f, vfrom, efrom, vdata, ns, N, p, E, L); });
   }, o.repeat);
   return {decompressContainer(xt, a, ks), l, t};
+}
+
+
+// Find pagerank of components in topological order (pull, CSR).
+// @param x  current graph
+// @param xt current transpose graph, with vertex-data=out-degree
+// @param q initial ranks (optional)
+// @param o options {damping=0.85, tolerance=1e-6, maxIterations=500}
+// @returns {ranks, iterations, time}
+template <class G, class H, class T=float>
+PagerankResult<T> pagerankLevelwise(const G& x, const H& xt, const vector<T> *q=nullptr, PagerankOptions<T> o={}) {
+  DiGraph<> w; DiGraph<int> wt;
+  return pagerankLevelwise(w, wt, x, xt, q, o);
 }
