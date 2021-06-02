559f4ef999f590da61637fc9c50e763c09505a95
Subhajit Sahu <wolfram77@gmail.com>
2021-06-02 17:04:15 +0530

    :skunk: w/ expt skip-comp

--- main.cxx
+++ main.cxx
@@ -10,23 +10,9 @@ using namespace std;
 
 
 
-template <class G, class H, class T>
-auto runPagerankCall(const char *name, const G& w, const H& wt, const G& x, const H& xt, const vector<T> *init, const vector<T> *ranks=nullptr) {
-  int repeat = name? 5:1;
-  auto a = pagerankLevelwise(w, wt, x, xt, init, {repeat});
-  auto e = absError(a.ranks, ranks? *ranks : a.ranks);
-  if (name) { print(xt); printf(" [%09.3f ms; %03d iters.] [%.4e err.] %s\n", a.time, a.iterations, e, name); }
-  return a;
-}
-
-template <class G, class H, class T>
-auto runPagerankCall(const char *name, const G& x, const H& xt, const vector<T> *init, const vector<T> *ranks=nullptr) {
-  DiGraph<> w; DiGraph<int> wt;
-  return runPagerankCall(name, w, wt, x, xt, init, ranks);
-}
-
 
 void runPagerankBatch(const string& data, bool show, int skip, int batch) {
+  int repeat = 5;
   vector<float>  ranksAdj;
   vector<float> *initStatic  = nullptr;
   vector<float> *initDynamic = &ranksAdj;
@@ -37,10 +23,11 @@ void runPagerankBatch(const string& data, bool show, int skip, int batch) {
     // Skip some edges (to speed up execution)
     if (!readSnapTemporal(x, s, skip)) break;
     auto xt = transposeWithDegree(x);
-    auto a1 = runPagerankCall(nullptr, x, xt, initStatic);
+    auto a1 = pagerankLevelwise(x, xt);
     auto ksOld    = vertices(x);
     auto ranksOld = move(a1.ranks);
 
+    // Read edges for this batch.
     auto y = copy(x);
     if (!readSnapTemporal(y, s, batch)) break;
     auto yt = transposeWithDegree(y);
@@ -48,11 +35,20 @@ void runPagerankBatch(const string& data, bool show, int skip, int batch) {
     ranksAdj.resize(y.span());
 
     // Find static pagerank of updated graph.
-    auto a2 = runPagerankCall("pagerankStatic", y, yt, initStatic);
+    auto a2 = pagerankLevelwise(y, yt, initStatic, {repeat});
+    auto e2 = l1Norm(a2.ranks, a2.ranks);
+    print(yt); printf(" [%09.3f ms; %03d iters.] [%.4e err.] pagerankStatic\n", a2.time, a2.iterations, e2);
+
+    // Find dynamic pagerank, with scaled-fill.
+    adjustRanks(ranksAdj, ranksOld, ksOld, ks, 0.0f, float(ksOld.size())/ks.size(), 1.0f/ks.size());
+    auto a3 = pagerankLevelwise(y, yt, initDynamic, {repeat});
+    auto e3 = l1Norm(a3.ranks, a2.ranks);
+    print(yt); printf(" [%09.3f ms; %03d iters.] [%.4e err.] pagerankDynamic\n", a3.time, a3.iterations, e3);
 
     // Find dynamic pagerank, with skip-comp and scaled-fill.
-    adjustRanks(ranksAdj, ranksOld, ksOld, ks, 0.0f, float(ksOld.size())/ks.size(), 1.0f/ks.size());
-    auto a3 = runPagerankCall("pagerankDynamic", x, xt, y, yt, initDynamic, &a2.ranks);
+    auto a4 = pagerankLevelwise(x, xt, y, yt, initDynamic, {repeat});
+    auto e4 = l1Norm(a4.ranks, a2.ranks);
+    print(yt); printf(" [%09.3f ms; %03d iters.] [%.4e err.] pagerankDynamic [skip-comp]\n", a4.time, a4.iterations, e4);
     x = move(y);
   }
 }
