912c5f561aec3d042e170c7023930136b83f1a5c
Subhajit Sahu <wolfram77@gmail.com>
2021-07-31 21:29:32 +0530

    :skunk: w/ a simple test

--- main.sh
+++ main.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-src="pagerank-levelwise-skip-unchanged-components"
+src="pagerank-levelwise-dynamic-validate-skip-unchanged-components"
 out="/home/resources/Documents/subhajit/$src.log"
 ulimit -s unlimited
 printf "" > "$out"
@@ -14,3 +14,4 @@ g++ -O3 main.cxx
 stdbuf --output=L ./a.out data/min1c1l.txt 2 2 true  2>&1 | tee -a "$out"
 stdbuf --output=L ./a.out data/min2c1l.txt 5 2 true  2>&1 | tee -a "$out"
 stdbuf --output=L ./a.out data/min3c3l.txt 13 5 true 2>&1 | tee -a "$out"
+stdbuf --output=L ./a.out data/min8c1l.txt 8 5 true 2>&1 | tee -a "$out"
